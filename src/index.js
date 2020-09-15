2d60ff94c33d72ff88e0665c029d802cf8b48917
ibrarsoomro <50750853+ibrarsoomro@users.noreply.github.com>
2020-09-15 20:36:07 +0500

    edit mini project

--- src/index.js
+++ src/index.js
@@ -2,29 +2,72 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 
+let curreDate = new Date()
+curreDate = curreDate.getHours();
+let greeting = "";
 
-const name = "ibrar"
-const img =     "https://picsum.photos/id/1/300/300"
-const img2 = "https://picsum.photos/id/100/300/300"
-const img3 = "https://picsum.photos/id/700/300/300"
-const img4 = "https://picsum.photos/id//500/300"
-const link = "https://github.com/ibrarsoomro"
+const cssStyle = {};
 
+if (curreDate >= 1 && curreDate < 12) {
+
+  greeting = "Good Morning";
+  cssStyle.color = "Green";
+}
+else if (curreDate >= 12 && curreDate        < 19) {
+  greeting = "Good after noon";
+  cssStyle.color = "Orange";
+}
+
+else {
+  greeting = "good night";
+  cssStyle.color = "Black";
+}
 
 ReactDom.render(<>
-  <h1 className="heading">my name is {name}</h1>
 
-<div className="image1"> 
-  <img src={img} alt="RandomImages" />
-  <img src={img2} alt="RandomImages" />
-  <img src={img3} alt="RandomImages" />
-
-  <a href={link} target="_github">
-  <img src={img4} alt="RandomImages" />
-  </a>
+<div className="headings">
+  <h1>Hello sir <span style={cssStyle}>{greeting}</span></h1>
   </div>
-</> ,
-  document.getElementById("root"))
+  
+  </>,
+  document.getElementById("root")
+)
+
+
+// const name = "ibrar"
+// const img = "https://picsum.photos/id/1/300/300"
+// const img2 = "https://picsum.photos/id/100/300/300"
+// const img3 = "https://picsum.photos/id/700/300/300"
+// const img4 = "https://picsum.photos/id//500/300"
+// const link = "https://github.com/ibrarsoomro"
+
+// const heading = {
+
+
+
+
+//   color: "navy",
+//   textAalign: "center",
+//   textTransform: "capitalize",
+//   margin: 70,
+//   fontFamily: "'Kumbh Sa', sans-serif"
+// }
+
+
+// ReactDom.render(<>
+//   <h1 className={heading}>my name is {name}</h1>
+
+//   <div className="image1">
+//     <img src={img} alt="RandomImages" />
+//     <img src={img2} alt="RandomImages" />
+//     <img src={img3} alt="RandomImages" />
+
+//     <a href={link} target="_github">
+//       <img src={img4} alt="RandomImages" />
+//     </a>
+//   </div>
+// </>,
+//   document.getElementById("root"))
 
 
 // const name = "ibrar ahmed";
