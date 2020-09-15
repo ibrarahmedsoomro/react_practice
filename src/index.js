109c0c0fd5e26bdf675bd66c3338819a185f23f9
ibrarsoomro <50750853+ibrarsoomro@users.noreply.github.com>
2020-09-15 00:37:51 -0700

    updated fonts and applied css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import './index.css';
+
 
 const name = "ibrar"
-const img = "https://picsum.photos/id/1/250/300"
+const img =     "https://picsum.photos/id/1/300/300"
 const img2 = "https://picsum.photos/id/100/300/300"
 const img3 = "https://picsum.photos/id/700/300/300"
 const img4 = "https://picsum.photos/id//500/300"
@@ -10,9 +12,9 @@ const link = "https://github.com/ibrarsoomro"
 
 
 ReactDom.render(<>
-  <h1 contentEditable="true">my name is {name}</h1>
+  <h1 className="heading">my name is {name}</h1>
 
-
+<div className="image1"> 
   <img src={img} alt="RandomImages" />
   <img src={img2} alt="RandomImages" />
   <img src={img3} alt="RandomImages" />
@@ -20,9 +22,8 @@ ReactDom.render(<>
   <a href={link} target="_github">
   <img src={img4} alt="RandomImages" />
   </a>
-
-</>
-  ,
+  </div>
+</> ,
   document.getElementById("root"))
 
 
