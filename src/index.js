7d97229bc817300e129589bf29ff1429e103eaf8
ibrarsoomro <50750853+ibrarsoomro@users.noreply.github.com>
2020-09-17 19:36:30 +0500

    update images and Css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 
+
+const name = "ibrar"
+const img = "https://picsum.photos/id/1/200/300"
+const img2 = "https://picsum.photos/id/100/200/300"
+const img3 = "https://picsum.photos/id/700/200/300"
+const img4 = "https://picsum.photos/id//200/300"
+const link = "https://github.com/ibrarsoomro"
+
+
 let curreDate = new Date()
 curreDate = curreDate.getHours();
 let greeting = "";
@@ -13,7 +22,7 @@ if (curreDate >= 1 && curreDate < 12) {
   greeting = "Good Morning";
   cssStyle.color = "Green";
 }
-else if (curreDate >= 12 && curreDate        < 19) {
+else if (curreDate >= 12 && curreDate < 19) {
   greeting = "Good after noon";
   cssStyle.color = "Orange";
 }
@@ -25,49 +34,28 @@ else {
 
 ReactDom.render(<>
 
-<div className="headings">
-  <h1>Hello sir <span style={cssStyle}>{greeting}</span></h1>
+  <div className="headings">
+    <h1>Hello sir <span style={cssStyle}>{greeting}</span></h1>
   </div>
+  <div>
+
+    <h1 className="headings">my name is {name}</h1>
+
+    <div className="image1">
+      <img src={img} alt="RandomImages" />
+      <img src={img2} alt="RandomImages" />
+      <img src={img3} alt="RandomImages" />
+
+      <a href={link} target="_github">
+        <img src={img4} alt="RandomImages" />
+      </a>
+    </div>
+  </div>
+
   
-  </>,
+</>,
   document.getElementById("root")
 )
-
-
-// const name = "ibrar"
-// const img = "https://picsum.photos/id/1/300/300"
-// const img2 = "https://picsum.photos/id/100/300/300"
-// const img3 = "https://picsum.photos/id/700/300/300"
-// const img4 = "https://picsum.photos/id//500/300"
-// const link = "https://github.com/ibrarsoomro"
-
-// const heading = {
-
-
-
-
-//   color: "navy",
-//   textAalign: "center",
-//   textTransform: "capitalize",
-//   margin: 70,
-//   fontFamily: "'Kumbh Sa', sans-serif"
-// }
-
-
-// ReactDom.render(<>
-//   <h1 className={heading}>my name is {name}</h1>
-
-//   <div className="image1">
-//     <img src={img} alt="RandomImages" />
-//     <img src={img2} alt="RandomImages" />
-//     <img src={img3} alt="RandomImages" />
-
-//     <a href={link} target="_github">
-//       <img src={img4} alt="RandomImages" />
-//     </a>
-//   </div>
-// </>,
-//   document.getElementById("root"))
 
 
 // const name = "ibrar ahmed";
